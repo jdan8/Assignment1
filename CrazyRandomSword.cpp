6db51f8ba56d1746fcee7ed48451fdf2e38d7ffe
Jay Daniels <31455510+jaydan8@users.noreply.github.com>
2019-02-03 18:47:56 -0500

    Updated Random Sword and Hammer Weapons
    Updated damage calculated for both of the weapons

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -9,7 +9,10 @@
 double CrazyRandomSword::hit(double armor){
 	
 	srand(time(NULL)); 
-double result = 0;
+	
+	int floorArmor = floor(armor);
+	int result = rand() % ((floorArmor / 3) + 2);
+
  double damage = hitPoints - (result);
     if (damage < 0) {
         return 0;
--- SimpleHammer.cpp
+++ SimpleHammer.cpp
@@ -5,7 +5,12 @@
 
 double SimpleHammer::hit(double armor){
     double damage;
-
+    if(armor < 30){
+		damage = hitPoints;
+	}
+	else{
+		damage = hitPoints - armor;
+	}
     if(damage < 0){
         return 0;
     }
